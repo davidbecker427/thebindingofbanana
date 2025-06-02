e4c34e82c31f8c4e0608fa44df858d54a3b37ae1
GhastlyClient2 <143698407+GhastlyClient2@users.noreply.github.com>
2025-06-02 13:13:00 +0200

    Enemy
    Één enemy type toegevoegd, kleine aanpassing aan de player gemaakt vanwege de enemy.

--- player.gd
+++ player.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
+class_name Player
+
+signal died
 
 @export var speed = 1000
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body is Enemy:
+		died.emit()
+		queue_free()
