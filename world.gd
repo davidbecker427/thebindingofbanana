4f488e14d145502dc65b43adb2bfbb61748c061e
Lucatj <143694438+Lucatj@users.noreply.github.com>
2025-06-27 22:52:28 +0200

    Finished Game-over GUI

--- world.gd
+++ world.gd
@@ -9,15 +9,22 @@ func _on_player_died():
 	print("game over")
 	get_node("/root/World/GameOverUI").visible = true
 
-func _on_enemy_died():
-	check_enemies_left()
+#func _on_enemy_died():
+	#check_enemies_left()
+#
+#func check_enemies_left():
+	#var enemies_left = get_tree().get_nodes_in_group("enemies").size()
+	#print("Enemies left:", enemies_left)
+	#if enemies_left == 0:
+		#_on_all_enemies_defeated()
+#
+#func _on_all_enemies_defeated():
+	#print("All enemies defeated!")
+	#get_node("/root/World/YouWinUI").visible = true
+	
 
-func check_enemies_left():
-	var enemies_left = get_tree().get_nodes_in_group("enemies").size()
-	print("Enemies left:", enemies_left)
-	if enemies_left == 0:
-		_on_all_enemies_defeated()
+func _on_restart_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://world.tscn")
 
-func _on_all_enemies_defeated():
-	print("All enemies defeated!")
-	get_node("/root/World/YouWinUI").visible = true
+func _on_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Startscherm.tscn")
